2105647c13a79f71977e6d7fd06e831bdce87589
Sam Trettin <trettinftw@gmail.com>
2020-06-22 23:32:50 -0700

    updated server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 5010;
 
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
-} else {
-    app.use(express.static('client/build'));
 }
+
+app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
