74da06d74f853a9a2f14de2daca5a9a90db068e1
Sam Trettin <trettinftw@gmail.com>
2020-07-06 17:54:49 -0700

    test

--- server.js
+++ server.js
@@ -15,25 +15,23 @@ if (process.env.NODE_ENV !== 'production'){
 
 const apiEndpoint = 'http://sandbox-api.brewerydb.com/v2';
 
-app.get('*', (req, res) => {
-    // res.send('hello heroku!');
-    const index = path.join(__dirname, 'client/build', 'index.html');
-    res.sendFile(index);
-});
-
 app.get('/beers/:beer', (req, res) => {
     const beerName = `${req.params.beer}`;
     // make request to https://sandbox-api.brewerydb.com/v2/key={process.env.BREW_KEY}
-    let list = axios.get(`${apiEndpoint}/search?key=${process.env.BREW_KEY}&type=beer&q=${beerName}`)
+    axios.get(`${apiEndpoint}/search?key=${process.env.BREW_KEY}&type=beer&q=${beerName}`)
         // // .data for axios payload
         .then(res => res)
         .catch((err) => console.error(err))
         // // .data for data from payload
         // .then(data => data.data)
         // .catch((err) => console.error(err))
-        .then(data => data);
-    
-    res.send(list);
+        .then(data => res.send(data));
+});
+
+app.get('*', (req, res) => {
+    // res.send('hello heroku!');
+    const index = path.join(__dirname, 'client/build', 'index.html');
+    res.sendFile(index);
 });
 
 app.listen(port, () => {
