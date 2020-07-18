61044c8064ecef419301c3f33e6a8f548f093f35
Sam Trettin <trettinftw@gmail.com>
2020-07-18 16:25:38 -0700

    test

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,10 +1,20 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import {GlobalContext} from '../context/GlobalState';
 
 import axios from 'axios';
 
 export const Search = () => {
     const { getBeers } = useContext(GlobalContext);
+
+    const handleClick = () => {
+      useEffect(() => {
+          let submit = document.getElementsByClassName('submit')[0];
+          submit.classList.add('handleClick');
+        return () => {
+          submit.classList.remove('handleClick');
+        }
+      }, [])
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -23,10 +33,10 @@ export const Search = () => {
 
     return (
       <div className="text-center mb-6 transition duration-1000 ease-in-out">
-        <h3 className="text-2xl sm:text-5xl text-yellow-700 py-3 px-2">Search for a Beer</h3>
+        <h3 className="text-2xl sm:text-5xl text-yellow-700 px-2">Search for a Beer</h3>
         <form onSubmit={handleSubmit}>
           <input className="shadow-md px-3 outline-none focus:bg-yellow-100 text-2md sm:text-2xl text-black rounded-l-md h-10 sm:h-12 w-8/12 sm:w-8/12" name="name" type="text" />
-          <input className="shadow-md h-10 sm:h-12 w-3/12 sm:w-2/12 text-2md sm:text-2xl text-black border-yellow-800 rounded-r-md outline-none bg-yellow-600 submit" type="submit" value="Submit"/>
+          <input onClick={handleClick} className="shadow-md h-10 sm:h-12 w-3/12 sm:w-2/12 text-2md sm:text-2xl text-black border-yellow-800 rounded-r-md outline-none bg-yellow-600 submit" type="submit" value="Submit"/>
         </form>
       </div>
     )
