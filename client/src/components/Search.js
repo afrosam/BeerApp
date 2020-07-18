7212fa045ba682b688a734639ba05d42bec82e83
Sam Trettin <trettinftw@gmail.com>
2020-07-18 16:55:42 -0700

    test

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -6,13 +6,6 @@ import axios from 'axios';
 export const Search = () => {
     const { getBeers } = useContext(GlobalContext);
 
-    const handleClick = () => {
-          let submit = document.getElementsByClassName('submit')[0];
-          submit.classList.add('handleClick');
-          setTimeout(250);
-          submit.classList.remove('handleClick');
-    }
-
     const handleSubmit = (e) => {
         e.preventDefault();
         const data = new FormData(e.target);
@@ -33,7 +26,7 @@ export const Search = () => {
         <h3 className="text-2xl sm:text-5xl text-yellow-700 px-2">Search for a Beer</h3>
         <form onSubmit={handleSubmit}>
           <input className="shadow-md px-3 outline-none focus:bg-yellow-100 text-2md sm:text-2xl text-black rounded-l-md h-10 sm:h-12 w-8/12 sm:w-8/12" name="name" type="text" />
-          <input onClick={handleClick} className="shadow-md h-10 sm:h-12 w-3/12 sm:w-2/12 text-2md sm:text-2xl text-black border-yellow-800 rounded-r-md outline-none bg-yellow-600 submit" type="submit" value="Submit"/>
+          <input className="shadow-md h-10 sm:h-12 w-3/12 sm:w-2/12 text-2md sm:text-2xl text-black border-yellow-800 rounded-r-md outline-none bg-yellow-600 submit" type="submit" value="Submit"/>
         </form>
       </div>
     )
